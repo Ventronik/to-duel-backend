c41a06d8b75833e8920322b4bf1ee4e9eda07363
Dustin Ruskell <druskell@gmail.com>
2018-06-07 15:36:24 -0700

    fixed single duel route with Roger

--- src/models/users.js
+++ src/models/users.js
@@ -228,13 +228,58 @@ function getAllUserDuels(users_id){
 
 function getOneDuel(id){
   return(
-    knex.raw(`select duels.id as duel_id, duels.u1_id as duel_u1id, duels.u2_id as duel_u2id, duels.start_time as duel_start, duels.end_time as duel_end, duels.u2_accepted as duel_accepted, duels.u1_confirmed as duel_confirmed, duels.winner_id as duel_winner_id, duels.rejected as duel_rejected, duels.archived as duel_archived, daily_history.id as dh_id, daily_history.dailies_id as dh_dailies_id, daily_history.completed as dh_completed, dailies.name as dailies_name, u1.first_name as user1, u2.first_name as user2 from duels
+    knex.raw(`select duels.id as duel_id, duels.u1_id as duel_u1id, duels.u2_id as duel_u2id, duels.start_time as duel_start, duels.end_time as duel_end, duels.u2_accepted as duel_accepted, duels.u1_confirmed as duel_confirmed, duels.winner_id as duel_winner_id, duels.rejected as duel_rejected, duels.archived as duel_archived, daily_history.id as dh_id, daily_history.dailies_id as dh_dailies_id, daily_history.completed as dh_completed, daily_history.created_at as dh_created, dailies.name as dailies_name, dailies.id as dailies_id, dailies.users_id as dailies_userid, u1.first_name as user1, u2.first_name as user2 from duels
       inner join duel_dailies on duels.id = duel_dailies.duel_id
       left join dailies on duel_dailies.dailies_id = dailies.id
       inner join daily_history on dailies.id = daily_history.dailies_id
       inner join users as u1 on duels.u1_id = u1.id
       inner join users as u2 on duels.u2_id = u2.id where duels.id = ${id};`
     )
+    .then(function({rows}){
+      // let result = data
+
+      const completed = rows.filter(ele => ele.dh_completed)
+
+      const inInterval = completed.filter(ele => new Date(ele.duel_start).getTime() < new Date(ele.dh_created).getTime() && new Date(ele.duel_end).getTime() >  new Date(ele.dh_created).getTime() )
+
+      const user1 = inInterval.reduce((users, row)=>{
+        if(users.hasOwnProperty(row.duel_u1id))
+            return { ...users , [row.duel_u1id]: [...users[row.duel_u1id],row] }
+          else
+            return {...users, [row.duel_u1id]: [row]}
+      }, {})
+
+      const user1formatted = {};
+      for(const u in user1){
+        user1formatted.name = u
+        user1formatted.dailies = user1[u].reduce((acc, ele) => {
+          if(acc.hasOwnProperty(ele.dailies_id))
+              return {...acc , [ele.dailies_id]: [...acc[ele.dailies_id], ele] }
+            else
+              return {...acc, [ele.dailies_id]: [ele]}
+        }, {})
+      }
+
+      const user2 = inInterval.reduce((users, row)=>{
+        if(users.hasOwnProperty(row.duel_u2id))
+            return {...users , [row.duel_u2id]: [...users[row.duel_u2id],row] }
+          else
+            return {...users, [row.duel_u2id]: [row]}
+      }, {})
+
+      const user2formatted = {};
+      for(const u in user2){
+        user2formatted.name = u
+        user2formatted.dailies = user2[u].reduce((acc, ele) => {
+          if(acc.hasOwnProperty(ele.dailies_id))
+              return {...acc , [ele.dailies_id]: [...acc[ele.dailies_id], ele] }
+            else
+              return {...acc, [ele.dailies_id]: [ele]}
+        }, {})
+      }
+
+      return {[id]: { [user1formatted.name]: user1formatted.dailies} }
+    })
   )
 }
 
