ea709c568ffb4457809291e6479036497b13b603
Dustin Ruskell <druskell@gmail.com>
2018-06-07 11:17:17 -0700

    more database changes

--- db/migrations/20180604111958_duels.js
+++ db/migrations/20180604111958_duels.js
@@ -3,12 +3,12 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.integer('u1_id').notNullable().references('users.id')
     table.integer('u2_id').notNullable().references('users.id')
-    table.dateTime('startTime').notNullable()
-    table.dateTime('endTime').notNullable()
+    table.dateTime('start_time').notNullable()
+    table.dateTime('end_time').notNullable()
     table.boolean('u2_accepted').notNullable().defaultTo(false)
     table.boolean('u1_confirmed').notNullable().defaultTo(false)
     table.boolean('rejected').notNullable().defaultTo(false)
-    table.integer('winnerId').defaultTo(null)
+    table.integer('winner_id').defaultTo(null)
     table.boolean('archived').notNullable().defaultTo(false)
     table.timestamps(true, true)
   });
