0356167519594d709578a55e5bce34666a540e86
Bryan Kai <bryankai@Bryans-MacBook-Pro.local>
2018-06-07 10:56:57 -0700

    Changed the createDailyHistory gateway conditionals to see if the typeof req.body.completed is undefined instead of seeing if the value was falsey

--- db/seeds/03_daily_history.js
+++ db/seeds/03_daily_history.js
@@ -5,15 +5,15 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex(TABLE_NAME).insert([
-        {id: 1, completed: true, dailies_id: 1, created_at: '2018-06-02T18:23:44.046Z'},
-        {id: 2, completed: true, dailies_id: 1, created_at: '2018-06-02T18:23:44.046Z'},
-        {id: 3, completed: true, dailies_id: 2, created_at: '2018-06-02T18:23:44.046Z'},
-        {id: 4, completed: true, dailies_id: 2, created_at: '2018-06-02T18:23:44.046Z'},
-        {id: 5, completed: true, dailies_id: 3},
-        {id: 6, completed: true, dailies_id: 3},
-        {id: 7, completed: true, dailies_id: 1},
-        {id: 8, completed: true, dailies_id: 2},
-        {id: 9, completed: true, dailies_id: 3},
+        {id: 1, completed: true, dailies_id: 1, created_at: '2018-06-07T08:23:44.046Z'},
+        {id: 2, completed: false, dailies_id: 1, created_at: '2018-06-07T09:23:44.046Z'},
+        {id: 3, completed: true, dailies_id: 1, created_at: '2018-06-07T02:23:44.046Z'},
+        {id: 4, completed: true, dailies_id: 2, created_at: '2018-06-03T18:23:44.046Z'},
+        {id: 5, completed: true, dailies_id: 3, created_at: '2018-06-03T08:23:44.046Z'},
+        {id: 6, completed: false, dailies_id: 3, created_at: '2018-06-07T09:23:44.046Z'},
+        {id: 7, completed: true, dailies_id: 1, created_at: '2018-06-06T18:23:44.046Z'},
+        {id: 8, completed: true, dailies_id: 2, created_at: '2018-06-03T18:23:44.046Z'},
+        {id: 9, completed: true, dailies_id: 3, created_at: '2018-06-07T10:23:44.046Z'},
       ]);
     })
     .then(() => {
